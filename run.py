c44ce414f3016b53dd56b0926381725336986368
Constantine Dev <1324252+constantinedev@users.noreply.github.com>
2021-12-18 23:43:34 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,20 +1,23 @@
-import re, io, sys, requests, subprocess, json, sqlite3, mysql.connector, datetime, schedule, time, socks
+import re, io, sys, requests, subprocess, json, sqlite3, mysql.connector, datetime, schedule, time
 
 from os.path import exists
 
 payload = {}
-headers = {}
+headers = {
+    "x-stand-news-client": "iOS",
+    "x-stand-news-version": "159",
+    "user-agent": "StandNews/1.0.14 (iPhone; iOS 15.2; Scale/2.00)",
+    "Content-Type": "application/json; charset=utf-8"
+}
 
-#url = 'https://api.thestandnews.com/api/app/v1/'
 x = datetime.datetime.now()
-dateTime = x.strftime("%d-%m-%Y_%H-%M-%S")
+dateDate = x.strftime('_%d-%m-%Y_')
+dateTime = x.strftime('%H-%M-%S')
 
-tor_proxy = requests.session()
-tor_proxy.proxies = {}
-tor_proxy.proxies['https'] = 'socks5h://127.0.0.1:9050'
-
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 9050)
-socks.socket = socks.socksocket
+tor_proxy = dict(
+    http = 'socks5://127.0.0.1:9050',
+    https = 'socks5://127.0.0.1:9050'
+)
 
 api_url = [
     #2
@@ -44,43 +47,42 @@ def  main():
     dir = 'output/'
     file_exists = exists(dir)
     api_count = len(api_url)
-    
-    
-    
         
     if file_exists == 'False':
-        subprocess.call('cp -R output_bk/ output/')
+        command_line = 'cp -R output_bk/ output/'
+        subprocess.call([command_line], shell=False, check=True)
+        return main()
     else:
         for count in range(0, api_count):
-            print('DONE! ' + api_url[count])
-            responser = requests.get(api_url[count], headers=headers, data=payload) #, headers=headers, data=payload
+            responser = requests.get(str(api_url[count]), headers=headers, data=payload) # proxies=tor_proxy,
             JsonData = responser.text
-            print(JsonData)
-            fileName = dateTime + '_' + api_name[count] + '.json'
+            fileName = api_name[count] + dateDate + dateTime + '.json'
             dir_checker = str(dir + api_name[count])
             checker = exists(dir_checker)
             if checker == True:
-                f = open(dir_checker + '/' + fileName, 'w', encoding="utf-8")
+                f = open(dir_checker + '/' + fileName, 'w', encoding="utf8")
                 f.writelines(JsonData)
                 f.close()
-
+                responser.close()
+            print('DONE! ' + api_url[count] + ':' + str(responser.status_code))
+            
 def payloader():
     dump_dir = 'output/article/pages/'
-    api_url = 'https://api.thestandnews.com/v1/article/pages/'
-    for countPage in range(1, 8000):
-        print('DONE! ' + api_url + str(countPage))
-        time.sleep(5)
-        responser = requests.get(api_url + str(countPage), headers=headers, data=payload)
+    api_url = 'https://api.thestandnews.com/v1/article/pages/' 
+    strartPageINT = input("start page: ")
+    for countPage in range(int(strartPageINT), 8000):
+        responser = requests.get(api_url + str(countPage), headers=headers, data=payload) # , proxies=tor_proxy
         payload_result = responser.text
-        print(payload_result)
-        fileName = dateTime + '_' + str(countPage) + '.json'
+        fileName = str(countPage) + dateDate + dateTime + '.json'
         checker = exists(dump_dir)
-        # if checker == True:
-        #     f = open('output/article/pages/' + fileName, 'w')
-        #     f.writelines(payload_result)
-        #     f.close()
-        #     responser.close()
-
+        if checker == True:
+            f = open('output/article/pages/' + fileName, 'w', encoding="utf8")
+            f.writelines(payload_result)
+            f.close()
+            responser.close()
+        print('DONE! ' + api_url + str(countPage) + ':' + str(responser.status_code))
+        #time.sleep(1200)
+        
 if __name__ == '__main__':
     main()
     payloader()
